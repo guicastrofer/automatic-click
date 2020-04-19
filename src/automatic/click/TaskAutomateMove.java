7189c212741e72d52fcd3acbce51c1ac43e7a172
Guilherme de Castro <42876310+guicastrofer@users.noreply.github.com>
2020-04-19 17:49:06 -0300

    Add automatic mouse moving function

---
+++ src/automatic/click/TaskAutomateMove.java
@@ -0,0 +1,23 @@
+package automatic.click;
+
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.util.Random;
+import java.util.TimerTask;
+
+public class TaskAutomateMove extends TimerTask{
+
+	@Override
+	public void run() {
+		Robot hal;
+		System.out.println("Running");
+		try {
+			Random random = new Random();
+			hal = new Robot();
+			hal.mouseMove(500+random.nextInt(), 800+random.nextInt());
+		} catch (AWTException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
