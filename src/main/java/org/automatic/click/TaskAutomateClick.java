4776463d2b02ce1f33fa92e7a62e6f4181fc63c1
Guilherme de Castro <guilherme.castro.fernandes@gmail.com>
2020-04-22 20:54:02 -0300

    Include pom.xml

---
+++ src/main/java/org/automatic/click/TaskAutomateClick.java
@@ -0,0 +1,21 @@
+package org.automatic.click;
+
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.util.TimerTask;
+
+public class TaskAutomateClick extends TimerTask {
+	@Override
+	public void run() {
+		Robot hal;
+		System.out.println("Running");
+		try {
+			hal = new Robot();
+			hal.mousePress(0);
+		} catch (AWTException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
